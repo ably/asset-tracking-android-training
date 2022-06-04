797f5d4c3b6a0bbeed2202855f4d8746070f2516
Kacper Kluka <k.kluka@nomtek.com>
2022-06-04 10:43:43 +0200

    Prepare a layout for the publisher app

--- publisher/src/main/java/com/ably/tracking/training/publisher/MainActivity.kt
+++ publisher/src/main/java/com/ably/tracking/training/publisher/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.ably.tracking.training.publisher
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupInitialViews()
+    }
+
+    private fun setupInitialViews() {
+        trackableIdTextView.text = getString(R.string.trackable_id_info, "")
+        trackableStatusTextView.text = getString(R.string.trackable_status_info, "")
+        trackableLatitudeTextView.text = getString(R.string.trackable_latitude_info, "")
+        trackableLongitudeTextView.text = getString(R.string.trackable_longitude_info, "")
     }
 }
