6b44ee3417d28491cfd669975c92d4331964a24e
Kacper Kluka <k.kluka@nomtek.com>
2022-06-04 10:42:27 +0200

    Add initial project setup

---
+++ subscriber/src/main/java/com/ably/tracking/training/subscriber/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ably.tracking.training.subscriber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
