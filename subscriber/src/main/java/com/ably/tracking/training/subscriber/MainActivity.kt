cbab7db38dbf15b46f35ceeda9609ad0cd097624
Kacper Kluka <k.kluka@nomtek.com>
2022-06-04 16:36:38 +0200

    Prepare a layout for the subscriber app

--- subscriber/src/main/java/com/ably/tracking/training/subscriber/MainActivity.kt
+++ subscriber/src/main/java/com/ably/tracking/training/subscriber/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.ably.tracking.training.subscriber
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var googleMap: GoogleMap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        prepareMap()
+        clearTrackableStatusInfo()
+    }
+
+    private fun clearTrackableStatusInfo() {
+        trackableStatusTextView.text = getString(R.string.trackable_status_info, "N/A")
+    }
+
+    private fun prepareMap() {
+        (mapFragmentContainerView.getFragment() as SupportMapFragment).let {
+            it.getMapAsync { map ->
+                map.uiSettings.isZoomControlsEnabled = true
+                googleMap = map
+            }
+        }
     }
 }
